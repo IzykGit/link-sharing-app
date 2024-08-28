cfe3ef6ccc982487a1bb3a3a133af2c6996fc1ca
Lance <lance_hemphill1234@yahoo.com>
2024-08-28 15:45:50 -0700

    added sign in button

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -68,6 +68,12 @@ const Navbar = ({ setSteps, steps, linkInfo }: { setSteps: Function, steps: Numb
             <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/>
         </svg>
     )
+
+    const signin = (
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#633CFF">
+            <path d="M480-120v-80h280v-560H480v-80h280q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H480Zm-80-160-55-58 102-102H120v-80h327L345-622l55-58 200 200-200 200Z"/>
+        </svg>
+    )
     
 
 
@@ -87,6 +93,7 @@ const Navbar = ({ setSteps, steps, linkInfo }: { setSteps: Function, steps: Numb
                     <button type='button' className={NavStyles.preview} onClick={() => router.push("/pages/previewPage")}>Preview</button>
 
                     {session && <button type="button" className={NavStyles.preview} onClick={() => handleSignOut()}>Sign Out</button>}
+                    {!session && <button type="button" className={NavStyles.preview_small} onClick={() => handleSignOut()}>{signin}</button>}
                 </div>
             </header>
         ) : (
@@ -103,6 +110,7 @@ const Navbar = ({ setSteps, steps, linkInfo }: { setSteps: Function, steps: Numb
                     <button type='button' className={NavStyles.preview_small} onClick={() => router.push("/pages/previewPage")}>{previewIcon}</button>
 
                     {session && <button type="button" className={NavStyles.preview_small} onClick={() => handleSignOut()}>{logout}</button>}
+                    {!session && <button type="button" className={NavStyles.preview_small} onClick={() => handleSignOut()}>{signin}</button>}
                 </div>
             </header>
         )
