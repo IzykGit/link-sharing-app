07095be9a842c3d1076291516d24fca77e910e94
Lance <lance_hemphill1234@yahoo.com>
2024-08-29 17:06:14 -0400

    fixed session check on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,14 @@ const index = () => {
   const { data: session } = useSession()
   const router = useRouter();
 
-  useEffect(() => {
-    if(session === null || !session.user) {
+
+  if(session === null || !session.user) {
       router.push("/auth/signin")
-    }
-    else {
-      router.push("/pages/home")
-    }
-  }, [session])
+  }
+  else {
+    router.push("/pages/home")
+  }
+
 
   return null;
 }
